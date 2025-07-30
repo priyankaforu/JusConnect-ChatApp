7dd29d09db8d5c812fef3edeb25e8a3c45a9907a
Priyanka Pudi <priyankapudi4u@gmail.com>
2025-07-30 17:41:46 +0530

    button size is changed

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -97,7 +97,7 @@ const MessageInput = () => {
         </div>
         <button
           type="submit"
-          className="btn btn-sm btn-circle"
+          className="btn btn-circle"
           disabled={!text.trim() && !imagePreview}
         >
           <Send size={22} />
